db4146fc1a2ba5ad8ff7b6f88a0c6109e02f4cf3
dkdiur383 <88004815+dkdiur383@users.noreply.github.com>
2023-06-06 21:12:42 -0700

    webflow

---
+++ .webflowrc.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+  siteId: 'gather-b3f7a5',
+  host: 'https://gather-b3f7a5.webflow.io/',
+  authToken: '5330d8f1306018ab39a701f176ad67deea8a308d32251ef082162435571bfb85'
+  // Add other optional configurations here, if needed
+};
+
+
